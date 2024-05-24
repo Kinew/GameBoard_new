268cdd388b521755a78306d1210d753c44999737
Kirill Tarasevich <capelin@mail.ru.ru>
2024-05-24 17:43:27 +0300

    the project update

--- game_board/GameBoard/models.py
+++ game_board/GameBoard/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Users(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class Article(models.Model):
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=64)
+    text = RichTextUploadingField()
+    category = models.CharField(max_length=12, choices=TYPE, default='tank')
+    dateCreations = models.DateTimeField(auto_now_add=True)
+
+
+class News(models.Model):
+    pass
