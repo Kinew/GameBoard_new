91e1b4f3b3b0ffe71c84bf8de6b0237a6c7598bd
Kirill Tarasevich <capelin@mail.ru.ru>
2024-06-07 00:02:34 +0300

    the project update

--- game_board/GameBoard/urls.py
+++ game_board/GameBoard/urls.py
@@ -3,6 +3,8 @@ from .views import *
 
 
 urlpatterns = [
+   path('confirm/', ConfirmUser.as_view(), name='confirm_user'),
+   path('profile', ProfileView.as_view(), name= 'profile'),
    path('post/', PostList.as_view()),
    path('post/<int:pk>/', PostDetail.as_view()),
    path('post/create/', PostCreate.as_view(), name='post_create'),
--- game_board/GameBoard/views.py
+++ game_board/GameBoard/views.py
@@ -65,3 +65,24 @@ class CommentDelete(LoginRequiredMixin, DeleteView):
     model = Comment
     template_name = 'comment_delete.html'
     success_url = reverse_lazy('post_list')
+
+
+class ConfirmUser(UpdateView):
+    model = User
+    context_object_name = 'confirm_user'
+
+    def post(self, request, *args, **kwargs):
+        if 'code' in request.POST:
+            user = User.objects.filter(code=request.POST['code'])
+            if user.exists():
+                user.update(is_active=True)
+                user.update(code=None)
+            else:
+                return  render(self.request,'users/invalid_code.html')
+            return redirect('account_login')
+
+
+class ProfileView(LoginRequiredMixin, templateView):
+    template_name = 'users/profile.html'
+
+
