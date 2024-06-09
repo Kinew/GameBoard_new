17a489c723d82f218a45953ff861c0df9acad1f1
Kirill Tarasevich <capelin@mail.ru.ru>
2024-06-09 22:26:39 +0300

    the project update

--- game_board/GameBoard/admin.py
+++ game_board/GameBoard/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from .models import *
 from django import forms
-from GameBoard.models import Post
-from django.contrib.flatpages.admin import FlatPageAdmin
-from django.contrib.flatpages.models import FlatPage
 from django.utils.translation import gettext_lazy as _
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
